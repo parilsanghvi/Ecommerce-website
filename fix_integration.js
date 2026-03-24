ec18594d73aa53e2050ec65320e95cd67a100ada
Paril Sanghvi <parilsanghvi@gmail.com>
2026-03-24 09:27:03 +0530

    Cleanup unnecessary files and update .gitignore

--- fix_integration.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('backend/tests/integration/review.test.js', 'utf8');
-
-code = code.replace(/console\.log\(res\.body\); /g, '');
-
-fs.writeFileSync('backend/tests/integration/review.test.js', code);
--- fix_mongoose.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('backend/controllers/productController.js', 'utf8');
-if (!code.includes("const mongoose = require('mongoose');")) {
-    code = `const mongoose = require('mongoose');\n` + code;
-    fs.writeFileSync('backend/controllers/productController.js', code);
-}
